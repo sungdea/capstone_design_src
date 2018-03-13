b6233282adb979773ba36778f32f04d7720957de
sungdea <201301475@inu.ac.kr>
2018-03-13 09:50:13 +0900

    voice_conversion
    voice_conversion

--- Android/CapstoneDesign/app/src/main/java/com/a2013myway/team/capstonedesign/MainActivity.java
+++ Android/CapstoneDesign/app/src/main/java/com/a2013myway/team/capstonedesign/MainActivity.java
@@ -2,12 +2,28 @@ package com.a2013myway.team.capstonedesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnInitListener {
 
+    private TextToSpeech myTTS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTTS = new TextToSpeech(this, this);
+    }
+    public void onInit(int status) {
+        String myText1 = "one of my favorite acotors";
+        String myText2 = "한글이 인식이 안됩니다!";
+        myTTS.speak(myText1, TextToSpeech.QUEUE_FLUSH, null);
+        myTTS.speak(myText2, TextToSpeech.QUEUE_ADD, null);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myTTS.shutdown();
     }
 }
