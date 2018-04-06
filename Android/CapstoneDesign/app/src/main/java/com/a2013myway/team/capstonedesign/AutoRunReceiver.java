55ea06548409ca72f7cf95aee6e4667726d08478
shotclock <shotclock@naver.com>
2018-04-06 13:43:52 +0900

    BOOTING CONNECTION
    부팅시 서비스 실행 및 MAC주소 저장되어 있는 경우 자동 연결
    TODO : 태그 값 읽기

--- Android/CapstoneDesign/app/src/main/java/com/a2013myway/team/capstonedesign/AutoRunReceiver.java
+++ Android/CapstoneDesign/app/src/main/java/com/a2013myway/team/capstonedesign/AutoRunReceiver.java
@@ -14,16 +14,15 @@ public class AutoRunReceiver extends BroadcastReceiver {
         String action = intent.getAction();
         if(action.equals("android.intent.action.BOOT_COMPLETED")){
             Intent boot_intent = new Intent(context,BluetoothLeService.class);
-            boot_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //boot_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startService(boot_intent);
-
         }
         else if(action.equals(Intent.ACTION_PACKAGE_REPLACED))
         {
             if(intent.getDataString().contains("com.a2013myway.team.capstonedesign"))
             {
                 Intent update_intent = new Intent(context,BluetoothLeService.class);
-                update_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //update_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startService(update_intent);
             }
         }
